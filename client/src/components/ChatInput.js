b07bae5cf8085503275f23b4ba92fb68011f5d2c
Nadav Shor <nadavs@nadavs-air.home>
2021-06-05 05:03:47 +0300

    enable sending massage with enter changed send key styling and fixed logged users image position

--- client/src/components/ChatInput.js
+++ client/src/components/ChatInput.js
@@ -23,7 +23,8 @@ const ChatInput = (props) => {
   }
 
   return (
-    <form noValidate autoComplete="off" className="chat-input">
+    <form noValidate autoComplete="off" className="chat-input" onKeyDown={e => {if (e.key === "Enter") {submitText()
+     e.preventDefault()}}}>
       <div >
         <TextField
           onChange={handleTextInput}
@@ -31,8 +32,8 @@ const ChatInput = (props) => {
           label="Chat text"
           style={{ margin: 8 }}
           value={text}
-          placeholder="Write here.."
           fullWidth
+          placeholder="Write here.."
           margin="normal"
           required={true}
           InputLabelProps={{
@@ -40,8 +41,10 @@ const ChatInput = (props) => {
           }}
         />
         <div className="send-button">
-          <Button variant="contained" color="primary" onClick={submitText}>
-            Send
+          <Button id="send-massage-button" variant="contained" color="primary" onClick={submitText}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" fill="currentColor" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"/>
+            </svg>
           </Button>
         </div>
       </div>
