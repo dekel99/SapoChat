7dec1cd7e600846c55063e6a3c9743c2e0e552f9
dekel <dekel99s@gmail.com>
2021-06-06 00:03:17 +0300

    minor changes

--- client/src/components/ChatInput.js
+++ client/src/components/ChatInput.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TextField from '@material-ui/core/TextField';
 import { Button } from "@material-ui/core";
+import SendIcon from '@material-ui/icons/Send';
 import "../styles/chatInput.css"
 
 
@@ -42,9 +43,7 @@ const ChatInput = (props) => {
         />
         <div className="send-button">
           <Button id="send-massage-button" variant="contained" color="primary" onClick={submitText}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" fill="currentColor" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"/>
-            </svg>
+            <SendIcon/>
           </Button>
         </div>
       </div>
--- client/src/components/UsersList.js
+++ client/src/components/UsersList.js
@@ -10,14 +10,15 @@ function UsersList(props) {
             <h4>Logged users:</h4>
             <div className="div-users">
                 <ul className="ul-users">
-                    {props.loggedUsers.map((user, index) => {
+                    {props.loggedUsers && props.loggedUsers.map((user, index) => {
                         return(
                             <div className="user-n-img" key={index}>  
                                 <img className="default-user-pic" src={user.profilePic ? user.profilePic : defPic} alt="profile-pic"/>
                                 <li className="user-name"><h5>{user.name}</h5></li>
                             </div>
                         )
-                    })}
+                    }).catch(err => console.log(err))
+                    }
                 </ul>
             </div>
         </div>
